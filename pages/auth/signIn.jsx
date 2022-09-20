fbf8a153903946bc8e2d904ec008d49550abd232
Chris Mittell <mittell@hotmail.co.uk>
2022-09-20 15:43:37 +0900

    fix: added Image component

--- pages/auth/signIn.jsx
+++ pages/auth/signIn.jsx
@@ -1,14 +1,14 @@
-/* eslint-disable @next/next/no-img-element */
 import React from 'react';
 import { getProviders, signIn as logIn } from 'next-auth/react';
 
 import Header from '../../components/Header';
+import Image from 'next/image';
 
 const signIn = ({ providers }) => {
 	const styles = {
 		outerContainer: 'mt-40',
 		innerContainer: 'flex flex-col items-center',
-		providerImage: 'w-52 object-cover',
+		providerImage: 'w-52',
 		infoText: 'text-small italic my-10 text-center',
 		signIn: 'bg-red-400 rounded-lg text-white p-3 hover:bg-red-500',
 	};
@@ -19,10 +19,12 @@ const signIn = ({ providers }) => {
 			<div className={styles.outerContainer}>
 				{Object.values(providers).map((provider) => (
 					<div key={provider.name} className={styles.innerContainer}>
-						<img
+						<Image
 							className={styles.providerImage}
 							src='https://www.google.co.jp/images/branding/googlelogo/2x/googlelogo_color_160x56dp.png'
 							alt='Google Logo'
+							height={112}
+							width={320}
 						/>
 						<p className={styles.infoText}>
 							This website is for learning purposes only!
